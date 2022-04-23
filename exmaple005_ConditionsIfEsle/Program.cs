6005bc4351026ae7bfc9643f8e73f5b7f0c93900
Pavel_Ziborov <zzipp@inbox.ru>
2022-04-23 13:50:16 +0300

    add Example 005 Contition If-Else

---
+++ exmaple005_ConditionsIfEsle/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ураа, это же Маша! Рад тебя видеть!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
