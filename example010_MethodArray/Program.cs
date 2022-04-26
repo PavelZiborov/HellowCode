cc92dff8f46226e5ca1a9f9051ca7b4d52a33a5d
Pavel_Ziborov <zzipp@inbox.ru>
2022-04-26 20:48:16 +0300

    Закончили лекуцию №2 по C#

---
+++ example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 21, 31, 41, 51, 61, 11, 81, 61 };
+int n = array.Length; // возвращает длину (количество элементов) массива array.
+int find = 61;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать цикл если значение true
+    }
+    // index = index + 1;
+    index++;
+}
---
+++ example011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // new int [10]; - создай массив из 10 элементов
+
+array[4] = 4;
+array[6] = 4;
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
